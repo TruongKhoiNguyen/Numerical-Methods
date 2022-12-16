6d2aef6a7ad012632171e35fdcc0fdfb24edfbf4
Truong Khoi Nguyen <truongkhoi31052002@gmail.com>
2022-12-16 23:16:34 +0700

    fix: gaussian elimination

--- System of Equations/gaussian_elimination.py
+++ System of Equations/gaussian_elimination.py
@@ -1,125 +1,74 @@
-Matrix = list[list[float]]
-
-# dimensions of matrix
-N = 3
-
-# function to get matrix content
+import numpy as np
+from tabulate import tabulate
 
 
-def gaussian_elimination(mat: Matrix) -> None:
-
-    # reduction into r.e.f.
-    singular_flag = forward_elimination(mat)
-
-    # if matrix is singular
-    if (singular_flag != -1):
-
-        print("Singular Matrix.")
-
-        # if the RHS of equation corresponding to
-        #   zero row  is 0, * system has infinitely
-        #   many solutions, else inconsistent*/
-        if (mat[singular_flag][N]):
-            print("Inconsistent System.")
-        else:
-            print("May have infinitely many solutions.")
-
-        return
-
-    # get solution to system and print it using
-    # backward substitution
-    backward_substitution(mat)
-
-# function for elementary operation of swapping two rows
+def print_matrix(mat):
+    print(tabulate(mat, tablefmt='simple_grid'))
 
 
-def swap_row(mat: Matrix, i: int, j: int) -> None:
+def gaussian_elimination(a):
+    print_matrix(a)
 
-    for k in range(N + 1):
+    n = len(a)
 
-        temp = mat[i][k]
-        mat[i][k] = mat[j][k]
-        mat[j][k] = temp
+    success = True
+    for k in range(n - 1):
+        # swap row to avoid 0
+        m = k
 
-# function to reduce matrix to r.e.f.
+        for j in range(k + 1, n):
+            if a[m][k] < a[j][k]:
+                m = j
 
+        if a[m][k] == 0:
+            success = False
+            break
 
-def forward_elimination(mat: Matrix) -> int:
-    for k in range(N):
+        a[[k, m]] = a[[m, k]]
 
-        # Initialize maximum value and index for pivot
-        i_max = k
-        v_max = mat[i_max][k]
+        if a[n - 1][n - 1] == 0:
+            success = False
+            break
 
-        # find greater amplitude for pivot if any
-        for i in range(k + 1, N):
-            if (abs(mat[i][k]) > v_max):
-                v_max = mat[i][k]
-                i_max = i
+        # eliminate rows below
+        for j in range(k + 1, n):
+            mjk = a[j][k] / a[k][k]
 
-        # if a principal diagonal element  is zero,
-        # it denotes that matrix is singular, and
-        # will lead to a division-by-zero later.
-        if not mat[k][i_max]:
-            return k    # Matrix is singular
+            for p in range(k + 1, n + 1):
+                a[j][p] = a[j][p] - mjk * a[k][p]
 
-        # Swap the greatest value row with current row
-        if (i_max != k):
-            swap_row(mat, k, i_max)
+        print_matrix(a)
 
-        for i in range(k + 1, N):
+    print('\n')
 
-            # factor f to set current row kth element to 0,
-            # and subsequently remaining kth column to 0 */
-            f = mat[i][k]/mat[k][k]
+    if success:
+        print('Solutions:')
+        x = np.zeros(n)
 
-            # subtract fth multiple of corresponding kth
-            # row element*/
-            for j in range(k + 1, N + 1):
-                mat[i][j] -= mat[k][j]*f
+        x[n - 1] = a[n - 1][n] / a[n - 1][n - 1]
 
-            # filling lower triangular matrix with zeros*/
-            mat[i][k] = 0
+        for i in reversed(range(0, n - 1)):
+            sum = 0
+            for j in range(i + 1, n):
+                sum += a[i][j] * x[j]
 
-        # print(mat);        //for matrix state
+            x[i] = 1 / a[i][i] * (a[i][n] - sum)
 
-    # print(mat);            //for matrix state
-    return -1
+        for i in range(n):
+            print(f'  x{i}: {x[i]}')
 
-# function to calculate the values of the unknowns
-
-
-def backward_substitution(mat: Matrix) -> None:
-
-    x = [None for _ in range(N)]    # An array to store solution
-
-    # Start calculating from last equation up to the
-    #  first */
-    for i in range(N-1, -1, -1):
-
-        # start with the RHS of the equation */
-        x[i] = mat[i][N]
-
-        # Initialize j to i+1 since matrix is upper
-        #  triangular*/
-        for j in range(i + 1, N):
-
-            # subtract all the lhs values
-            # except the coefficient of the variable
-            # whose value is being calculated */
-            x[i] -= mat[i][j]*x[j]
-
-        # divide the RHS by the coefficient of the
-        #  unknown being calculated
-        x[i] = (x[i]/mat[i][i])
-
-    print("\nSolution for the system:")
-    for i in range(N):
-        print("{:.8f}".format(x[i]))
-
-# Driver program
+    else:
+        print('No unique solution exist')
 
 
-# input matrix
-mat = [[3.0, 2.0, -4.0, 3.0], [2.0, 3.0, 3.0, 15.0], [5.0, -3, 1.0, 14.0]]
-gaussian_elimination(mat)
+def main():
+    mat = np.array([[0, 8, 2, -7],
+                    [3, 5, 2, 8],
+                    [6, 2, 8, 26]],
+                   dtype=np.float64)
+
+    gaussian_elimination(mat)
+
+
+if __name__ == '__main__':
+    main()
